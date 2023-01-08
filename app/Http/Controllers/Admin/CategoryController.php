3bbac14e9943a85e64b73efa42287e427547de9b
maher-tba <63453080+maher-tba@users.noreply.github.com>
2023-01-08 07:48:21 +0300

    change name of project

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use function Sodium\add;
+
+class CategoryController extends Controller
+{
+
+    public function __construct()
+    {
+        if (Auth::check()) {
+            $this->authorize('admin-access'); //AuthServiceProvider  permission
+        }
+        else{
+            return redirect()->route('login');
+        }
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::with('categories')->get();
+        return view('admin.categories.index',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd('create category');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validate the given request
+        $data = $this->validate($request, [
+            'title' => 'required|max:255',
+            'user_id'=> 'required',
+        ]);
+        $user = User::findOrFail($data['user_id']);
+        $user->categories()->create([
+            'title' => $data['title'],
+            'is_hidden' => false,
+            'author' => Auth::user()->name,
+        ]);
+        session()->flash('status', ' Created!');
+
+        return redirect('admin/categories');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $Category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $Category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $Category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $Category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $Category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $Category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $Category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $Category)
+    {
+        //
+    }
+
+}
